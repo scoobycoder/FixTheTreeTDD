891800b81becd222011e7841ffd20850eee69e3e
Nate <nathan_halterman@gap.com>
2016-06-21 16:21:53 -0400

    refactor to move tests to proper spec section and to create before block

--- spec/tree_spec.rb
+++ spec/tree_spec.rb
@@ -1,20 +1,11 @@
 require 'rspec'
 require './lib/tree'
 
+
 describe 'Tree' do
+
   it 'should be a Class' do
     expect(Tree.is_a? Class).to eq(true)
-  end
-
-  it 'should tell you if it has apples' do
-    apple_tree = AppleTree.new
-    apple_tree.add_apples
-    expect(apple_tree.any_apples?).to eq(true)
-  end
-
-  it 'should tell you if it has no apples' do
-    apple_tree = AppleTree.new
-    expect(apple_tree.any_apples?).to eq(false)
   end
 
 end
@@ -23,4 +14,16 @@ describe 'Fruit' do
 end
 
 describe 'Apple' do
+  before do
+    @apple_tree = AppleTree.new
+  end
+
+  it 'should tell you if it has apples' do
+    @apple_tree.add_apples
+    expect(@apple_tree.any_apples?).to eq(true)
+  end
+
+  it 'should tell you if it has no apples' do
+    expect(@apple_tree.any_apples?).to eq(false)
+  end
 end
